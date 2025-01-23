1bcd3e9c7ca0611680a810c7fc2057c5bba79840
docsdt <docsdevkraft@gmail.com>
2025-01-23 13:13:13 +0530

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUpload, faTable, faAddressBook, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 import UpdateElectron from '@/components/update';
 
@@ -16,28 +18,59 @@ import './index.css';
 const App = () => {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
-        <nav className="bg-white-600">
-          <ul className="flex space-x-4 p-4 text-white list-none ">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/delivery">Upload Delivery Data</Link></li>
-            <li><Link to="/delivery-table">Delivery Data</Link></li>
-            <li><Link to="/master">Upload Master Data</Link></li>
-            <li><Link to="/master-table">Master Data</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+      <div className="min-h-screen flex">
+        <nav className="w-64 bg-gray-800 text-white flex flex-col">
+          <ul className="space-y-4 p-4 list-none">
+            <li>
+              <Link to="/" className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
+                <FontAwesomeIcon icon={faHome} className="text-white" />
+                <span className="text-white">Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/delivery" className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
+                <FontAwesomeIcon className="text-white" icon={faUpload} />
+                <span className="text-white">Upload Delivery Data</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/delivery-table" className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
+                <FontAwesomeIcon className="text-white" icon={faTable} />
+                <span className="text-white">Delivery Data</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/master" className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
+                <FontAwesomeIcon className="text-white" icon={faUpload} />
+                <span className="text-white">Upload Master Data</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/master-table" className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
+                <FontAwesomeIcon className="text-white" icon={faTable} />
+                <span className="text-white">Master Data</span>
+              </Link>
+            </li>
+            {/* <li>
+              <Link to="/contact" className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
+                <FontAwesomeIcon icon={faEnvelope} />
+                <span>Contact</span>
+              </Link>
+            </li> */}
           </ul>
         </nav>
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/delivery" element={<Delivery />} />
-          <Route path="/delivery-table" element={<DeliveryTable />} />
-          <Route path="/master" element={<Master />} />
-          <Route path="/master-table" element={<MasterTablePage />} />
-          {/* {/* <Route path="/about" element={<AboutPage />} /> */}
-        </Routes>
+        <div className="flex-1 bg-gray-100 p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/delivery-table" element={<DeliveryTable />} />
+            <Route path="/master" element={<Master />} />
+            <Route path="/master-table" element={<MasterTablePage />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </div>
       </div>
-
     </Router>
   );
 };
